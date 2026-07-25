42caece838fb15f33eacc0bc4b495653b4270879
伤心拖把 <yiwensimon.ding@gmail.com>
2026-07-25 13:46:47 +0800

    1

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public sealed class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
