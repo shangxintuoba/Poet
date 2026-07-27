8ddc556b4b9a14c9cbf13c53574aeb3a96698859
伤心拖把 <yiwensimon.ding@gmail.com>
2026-07-27 21:50:12 +0800

    Scene

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,6 +2,35 @@ using UnityEngine;
 
 public sealed class GameManager : MonoBehaviour
 {
+
+    public enum Scene_Large
+    {
+        BookStore,
+        Home,
+        Park,
+        Restaurant,
+        McDonald,
+        Bar,
+        Club,
+        Cinema,
+        Street_Home,
+        Street_Book,
+        Street_Downtown,
+        Street_NewDistrict,
+        Sea,
+        Supermarket,
+        CBDMarket,
+        EditorOffice,
+        HerHome,
+        Gallery,
+        Museum,
+        Subway,
+    }
+
+    public int Date;
+
+
+
     public static GameManager Instance { get; private set; }
 
     private void Awake()
