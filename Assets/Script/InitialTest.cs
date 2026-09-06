fa244d52d369402221a1b61b13009a82ee0bb0d2
伤心拖把 <yiwensimon.ding@gmail.com>
2026-09-06 17:14:11 +0800

    hint panel and initial test(half)

---
+++ Assets/Script/InitialTest.cs
@@ -0,0 +1,27 @@
+using Ink.Parsed;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InitialTest : MonoBehaviour
+{
+    public TextMeshProUGUI TextArea;
+    private int temperature;
+    private int moisture;
+    private int age;
+    private int reality;
+    private int language;
+    private int core;
+    private int willlpower;
+
+
+
+
+
+
+
+
+
+
+
+}
